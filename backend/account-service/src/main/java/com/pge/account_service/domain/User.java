4817ccce2ac9094de2734b9957681ea15848f8a6
Rodrigo Soares <adsrodsoares@gmail.com>
2026-07-11 15:29:04 -0300

    UserType e User

---
+++ backend/account-service/src/main/java/com/pge/account_service/domain/User.java
@@ -0,0 +1,32 @@
+import com.pge.account_service.domain.UserType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private UserType type;
+
+}
