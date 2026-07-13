ded4c3e2368922b25ccb879a8b37c6eec55e78ab
Rodrigo Soares <adsrodsoares@gmail.com>
2026-07-13 12:37:23 -0300

    Ride Controller - endpoint e body da request

---
+++ backend/ride-service/src/main/java/com/pge/ride_service/controller/RideController.java
@@ -0,0 +1,27 @@
+package com.pge.ride_service.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pge.ride_service.RideService;
+import com.pge.ride_service.domain.Ride;
+
+@RestController
+@RequestMapping("/rides")
+public class RideController {
+
+    private final RideService rideService;
+
+    public RideController(RideService rideService) {
+        this.rideService = rideService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Ride> create(@RequestBody Ride ride) {
+        Ride createdRide = rideService.createRide(ride);
+        return ResponseEntity.ok(createdRide);
+    }
+}
