9842ee13b6030e4de8a868e06d18562ebea01fa3
Rodrigo Soares <adsrodsoares@gmail.com>
2026-07-12 21:35:09 -0300

    Serviço de corrida

---
+++ backend/ride-service/src/main/java/com/pge/ride_service/RideServiceApplication.java
@@ -0,0 +1,17 @@
+package com.pge.ride_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class RideServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideServiceApplication.class, args);
+	}
+
+}
